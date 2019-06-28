968df52b6073473b1cbdf82ee85ff32d8ede3bd6
Chiwon <chiwon99881@gmail.com>
2019-06-28 13:39:41 +0900

    First Query for Auth as hooks

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 ## 두번째
 
 - 필요한 Router들 체크 후 만들고 로그인 여부에 따라 다른 Routing을 해줘야 함
+
+## 세번째
+
+- 이제 LocalState를 설정해줘야함 우리가 필요한건 로그인 로그아웃 여부만 필요함
+- ApolloClient를 통해서 우리가 만든 서버의 uri와 연결해주고
+- LocalState를 만들어서 logout login 기능을 LocalState로 만들어줌
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -5,8 +5,17 @@ export default createGlobalStyle`
     ${reset};
     * {
         box-sizing:border-box;
+    }
+    body {
+        background-color: ${props => props.theme.bgColor};
+        color:${props => props.theme.blackColor};
     } 
+    a {
+        color: ${props => props.theme.blueColor};
+        text-decoration: none;
+    }
     
 `;
 
 //* {box-sizing:border-box} 박스형태의 스타일들의 사이즈를 테두리까지로 설정하는것 원래는 테두리는 계산안함
+//props는 어디서 오냐 App에 보면 ThemeProvider로 부터 GlobalStyles 가 감싸져있으니까 props가 전달됨
